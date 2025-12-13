ecb22d666fdfab257fda2ddfb4ce46379ad5f67e
123456 <hoochanlon@outlook.com>
2025-12-13 14:42:40 +0800

    asd

--- astro.config.ts
+++ astro.config.ts
@@ -10,11 +10,25 @@ import UnoCSS from 'unocss/astro'
 import devtoolsJson from 'vite-plugin-devtools-json'
 import { themeConfig } from './src/.config'
 
+// 根据环境变量动态设置 base 路径
+// 如果设置了 ASTRO_BASE 环境变量，使用该值；否则根据 DEPLOY_ENV 判断
+// DEPLOY_ENV=deno 时使用 '/'，其他情况使用 '/fonts/'
+const getBasePath = () => {
+  if (process.env.ASTRO_BASE) {
+    return process.env.ASTRO_BASE
+  }
+  if (process.env.DEPLOY_ENV === 'deno') {
+    return '/'
+  }
+  // 默认使用 GitHub Pages 路径
+  return '/fonts/'
+}
+
 // https://astro.build/config
 export default defineConfig({
   site: themeConfig.site.website,
   prefetch: true,
-  base: '/fonts/',
+  base: getBasePath(),
   vite: {
     plugins: [
       // eslint-disable-next-line ts/ban-ts-comment
